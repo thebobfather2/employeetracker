8adce811d6749b9933e53bc93b8a63a4aeccf3d1
thebobfather2 <107475188+thebobfather2@users.noreply.github.com>
2023-04-13 18:09:38 -0400

    updated with tutor

---
+++ index.js
@@ -0,0 +1,36 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const {printTable} = require('console-table-printer')
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      password: 'password',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
+
+  function mainMenu() {
+    inquirer.prompt({
+        type: "list",
+        message: "What would you like to do?",
+        name: "selection",
+        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
+    })
+    .then(response => {
+        if(response.selection === "view all departments") {
+            viewDepartments()
+        }
+    })
+  };
+
+  function viewDepartments() {
+    db.query("select * from department", (err, data) => {
+        printTable(data);
+        mainMenu();
+    })
+  }
+
+  mainMenu();
